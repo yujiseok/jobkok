3b5d4ce583d82ee97e27d8e7c42c09167cf27163
yujiseok <koesijuy@gmail.com>
2023-03-23 15:24:02 +0900

    env: `svg` 사용을 위한 플러그인 설치 (#16)
    - vite-plugin-svgr
    - @svgr/plugin-jsx
    - @svgr/plugin-svgo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
+import { ReactComponent as Test } from "@/svg/test.svg";
 import Button from "@components/Common/Button";
-
 const Home = () => {
   return (
     <div className="text-xl font-extrabold text-rose-300">
       Hello World
       <br />
       <Button />
+      <div>
+        <Test className="h-6 w-6" />
+      </div>
     </div>
   );
 };

6c0067e21fd9c4edf038ea680a9c6dd1be9c8329
hyerani <hran9404@naver.com>
2023-03-31 01:58:35 +0900

    chore: 불필요 내용 삭제

--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -2,7 +2,7 @@ import type { AxiosResponse } from "axios";
 import type { User } from "@pages/SignIn";
 import { client } from "./axios";
 
-// 어려워욤.. 모르겠어욤..
+// 진행중..
 export const postSignIn = async () => {
   try {
     const { data }: AxiosResponse<User> = await client({
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import type { ShowPw } from "@pages/SignIn";

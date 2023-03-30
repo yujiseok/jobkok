f495897808f6949e309ec7dba3dca561061177f7
yujiseok <koesijuy@gmail.com>
2023-03-30 20:55:04 +0900

    feat: `Pretendard` 폰트 추가
    - 에셋 받은 후 교체

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ["Pretendard"],
+      },
       boxShadow: {
         job: " 0px 2px 5px rgba(0, 19, 51, 0.1), 0px 0px 0px rgba(0, 19, 51, 0.1);",
       },

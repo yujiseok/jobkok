1957627efb94b6dd4b19acb5bad6ac634bcf66cf
yujiseok <koesijuy@gmail.com>
2023-03-31 13:31:32 +0900

    feat: 인재 관리 페이지 마크업 (#57)

--- src/pages/TalentManagement/index.tsx
+++ src/pages/TalentManagement/index.tsx
@@ -2,10 +2,18 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { Link } from "react-router-dom";
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css/pagination";
-import "./index.css";
+import "./swiper.css";
+import { ReactComponent as ArchiveTick } from "@/assets/svg/archive-tick.svg";
+import { ReactComponent as Calendar } from "@/assets/svg/calendar.svg";
+import { ReactComponent as ChevronRight } from "@/assets/svg/chevron-right.svg";
+import { ReactComponent as HeartMemoji } from "@/assets/svg/heart-memoji.svg";
+import { ReactComponent as Pin } from "@/assets/svg/pin.svg";
+import { ReactComponent as Rocket } from "@/assets/svg/rocket.svg";
+import { ReactComponent as Smile } from "@/assets/svg/Smile.svg";
+import { ReactComponent as Stats } from "@/assets/svg/stats.svg";
 import useDnD from "@/lib/hooks/useDnD";
-import Heading from "@components/Talent/Heading";
+import NumberBadge from "@components/Talent/NumberBadge";
+import ProcedureBadge from "@components/Talent/ProcedureBadge";
 
 const mockData = [
   {
@@ -52,70 +60,85 @@ const mockData = [
   },
 ];
 
-const numberArr = Array(9)
+const numberArr = Array(6)
   .fill(1)
   .map((v, i) => i + 1);
+
+const randomNumber1 = [...numberArr].sort(() => 0.5 - Math.random());
+const randomNumber2 = [...numberArr].sort(() => 0.5 - Math.random());
 
 const TalentManagement = () => {
   const [data, onDragEnd] = useDnD(mockData);
 
   return (
     <>
-      <section className="absolute top-16 left-0 flex w-full flex-col gap-8 bg-red-100 py-12">
-        <div className="flex items-center justify-between ">
-          <select className="appearance-none outline-none">
-            <option selected>스마트스토어 상세페이지 디자이너 지원서 폼</option>
-            <option>안녕하세요</option>
-            <option>반갑습니다</option>
-          </select>
+      <section className="absolute top-16 left-0  h-[25rem] w-full bg-blue-400 py-12 text-gray-0">
+        <div className="mx-auto flex max-w-7xl flex-col gap-8">
+          <div className="flex items-center justify-between ">
+            <select className="SubHead2Semibold appearance-none bg-transparent outline-none">
+              <option selected>
+                스마트스토어 상세페이지 디자이너 지원서 폼
+              </option>
+              <option>안녕하세요</option>
+              <option>반갑습니다</option>
+            </select>
 
-          <button className="rounded-md bg-slate-400 py-2 px-5">
-            폼 마감하기
-          </button>
-        </div>
-
-        <div className="text-center">
-          <h1>
-            안녕하세요 <span>잡콕미술학원</span>님!
-          </h1>
-          <div>현재까지 지원현황을 간략히 한 눈에 보여드립니다.</div>
-        </div>
-
-        <div className="flex justify-center text-center">
-          <div className="px-10">
-            <div>
-              <span>4</span>명
-            </div>
-            <div>오늘의 인재수</div>
+            <button className="SubHead2Semibold rounded-md bg-blue-50 py-2 px-5 text-blue-400">
+              폼 마감하기
+            </button>
           </div>
 
-          <div className="border-r border-l border-black px-10">
-            <div>
-              D-<span>9</span>
+          <div className="text-center">
+            <h1 className="Head2Semibold mb-1">
+              안녕하세요 <span>잡콕미술학원</span>님!
+            </h1>
+            <div className="Head3Semibold">
+              현재까지 지원현황을 간략히 한 눈에 보여드립니다.
             </div>
-            <div>서류접수 마감일</div>
           </div>
 
-          <div className="px-10">
-            <div>
-              <span>24</span>명
+          <div className="flex justify-center text-center">
+            <div className="px-36">
+              <div className="Head1Bold mb-1">
+                <span>4</span>명
+              </div>
+              <div>오늘의 인재수</div>
             </div>
-            <div>총 지원 인재수</div>
+
+            <div className="border-r border-l border-gray-0 px-36">
+              <div className="Head1Bold mb-1">
+                D-<span>9</span>
+              </div>
+              <div>서류접수 마감일</div>
+            </div>
+
+            <div className="px-36">
+              <div className="Head1Bold mb-1">
+                <span>24</span>명
+              </div>
+              <div>총 지원 인재수</div>
+            </div>
           </div>
         </div>
       </section>
 
-      <section className="mt-96">
+      <section className="mt-[34rem]">
         <div>
-          <Heading className="mb-12">
-            <span>📌</span>잡콕에서 추천하는 엄선한 인재들 입니다!
-          </Heading>
+          <h4 className="Head3Semibold mb-12 flex items-center gap-1">
+            <Pin className="-scale-x-100" />
+            잡콕에서 추천하는 인재들 입니다!
+          </h4>
         </div>
 
         <div className="flex flex-col gap-8">
-          <div className="relative flex  gap-4">
-            <div className="flex-[0.3] rounded-xl bg-slate-300 shadow-job">
-              아아아아
+          <div className="relative flex gap-4 p-1">
+            <div className="relative flex-[0.3] rounded-xl bg-blue-400 px-4 py-6 text-gray-0 shadow-job">
+              <p className="SubHead1Semibold mb-3">잡콕인재추천</p>
+              <p className="SubHead2Medium">
+                설정하신 우대사항 란에
+                <br /> 가장많이 기입한 지원자가 추천됩니다.
+              </p>
+              <Rocket className="absolute right-4 bottom-2" />
             </div>
             <Swiper
               slidesPerView={3}
@@ -128,18 +151,40 @@ const TalentManagement = () => {
               slidesPerGroup={3}
               className="flex-1"
             >
-              {numberArr.map((value) => (
+              {numberArr.map((value, i) => (
                 <SwiperSlide key={value}>
-                  <div className="h-48 rounded-xl bg-slate-300 shadow-job">
-                    Slide {value}
+                  <div className="relative h-48 rounded-xl bg-gray-0 px-4 py-6 shadow-job">
+                    <div className="mb-3 flex justify-between">
+                      <div className="flex items-center gap-[0.375rem]">
+                        <div className="SubHead1Semibold">김잡콕 {value}</div>
+                        <ProcedureBadge>서류검토</ProcedureBadge>
+                      </div>
+                    </div>
+                    <div>badge</div>
+                    <time
+                      dateTime={new Date().toLocaleDateString()}
+                      className="Caption1Medium absolute bottom-5 text-gray-300"
+                    >
+                      {new Date().toLocaleDateString()}
+                    </time>
+                    <img
+                      src={`/assets/svg/thumbs-${randomNumber1[i]}.svg`}
+                      alt={`thumbs-${randomNumber1[i]}`}
+                      className="absolute bottom-0 right-1"
+                    />
                   </div>
                 </SwiperSlide>
               ))}
             </Swiper>
           </div>
-          <div className="relative flex gap-4">
-            <div className="flex-[0.3] rounded-xl bg-slate-300 shadow-job">
-              아아아아
+          <div className="relative flex gap-4 p-1">
+            <div className="relative flex-[0.3] rounded-xl bg-banner-teal-500 px-4 py-6 text-gray-0 shadow-job">
+              <p className="SubHead1Semibold mb-3">잡콕인재추천</p>
+              <p className="SubHead2Medium">
+                설정하신 우대사항 란에
+                <br /> 가장많이 기입한 지원자가 추천됩니다.
+              </p>
+              <Stats className="absolute right-4 bottom-2" />
             </div>
             <Swiper
               slidesPerView={3}
@@ -152,10 +197,27 @@ const TalentManagement = () => {
               slidesPerGroup={3}
               className="flex-1"
             >
-              {numberArr.map((value) => (
+              {numberArr.map((value, i) => (
                 <SwiperSlide key={value}>
-                  <div className="h-48 rounded-xl bg-slate-300 shadow-job">
-                    Slide {value}
+                  <div className="relative h-48 rounded-xl bg-gray-0 px-4 py-6 shadow-job">
+                    <div className="mb-3 flex justify-between">
+                      <div className="flex items-center gap-[0.375rem]">
+                        <div className="SubHead1Semibold">김잡콕 {value}</div>
+                        <ProcedureBadge>서류검토</ProcedureBadge>
+                      </div>
+                    </div>
+                    <div>badge</div>
+                    <time
+                      dateTime={new Date().toLocaleDateString()}
+                      className="Caption1Medium absolute bottom-5 text-gray-300"
+                    >
+                      {new Date().toLocaleDateString()}
+                    </time>
+                    <img
+                      src={`/assets/svg/thumbs-${randomNumber2[i]}.svg`}
+                      alt={`thumbs-${randomNumber2[i]}`}
+                      className="absolute bottom-0 right-1"
+                    />
                   </div>
                 </SwiperSlide>
               ))}
@@ -163,17 +225,22 @@ const TalentManagement = () => {
           </div>
         </div>
       </section>
-      <br />
 
       {/* 채용진행현황 */}
-      <section>
-        <Heading className="mb-6">채용 진행 현황</Heading>
+      <section className="mt-20">
+        <h4 className="Head3Semibold mb-6">채용 진행 현황</h4>
         <div className="flex justify-between">
-          <p>
+          <p className="SubHead1Semibold text-gray-400">
             한 눈에 칸반보드에서 인재 현황을 확인해보세요. 인재카드를
             Drag&Drop을 통해 자유롭게 이동해보세요.
           </p>
-          <Link to="/talent/status">관리하기</Link>
+          <Link
+            to="/talent/status"
+            className="SubHead1Medium flex items-center gap-2 text-gray-400"
+          >
+            관리하기
+            <ChevronRight />
+          </Link>
         </div>
 
         {/* <div className="flex">
@@ -190,24 +257,39 @@ const TalentManagement = () => {
         {/* 칸반 */}
 
         <DragDropContext onDragEnd={onDragEnd}>
-          <div className="mt-8 grid grid-cols-3 gap-16">
-            {data.map((section) => (
-              <Droppable key={section.id} droppableId={section.id}>
+          <div className="mt-8 grid grid-cols-3 gap-6">
+            {data.map(({ id, title, tasks }) => (
+              <Droppable key={id} droppableId={id}>
                 {(provided) => (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className="bg-slate-400 p-4"
+                    className="rounded-xl bg-slate-400 pl-8 pr-4 pb-12"
                   >
-                    <div>
-                      {section.title} <span>{section.tasks.length}</span>
+                    <div
+                      className="flex items-center justify-between pr-8
+                    "
+                    >
+                      <div className="flex items-center py-5">
+                        <span className="SubHead1Semibold">{title}</span>{" "}
+                        <NumberBadge id={id}>{tasks.length}</NumberBadge>
+                      </div>
+                      {id === "서류 검토" ? (
+                        <button>
+                          <Calendar />
+                        </button>
+                      ) : id === "최종 조율" ? (
+                        <button className="rounded-md border border-blue-500 bg-gray-0 px-5 py-[0.3438rem] text-blue-500">
+                          채용 확정
+                        </button>
+                      ) : null}
                     </div>
 
                     <div
-                      className="flex max-h-40 flex-col gap-4 overflow-y-auto overflow-x-hidden bg-amber-300 p-4 
+                      className="flex max-h-40 flex-col gap-4 overflow-y-auto overflow-x-hidden  pr-3
                     "
                     >
-                      {section.tasks.map((task, index) => (
+                      {tasks.map((task, index) => (
                         <Draggable
                           key={task.id}
                           draggableId={task.id}
@@ -215,10 +297,10 @@ const TalentManagement = () => {
                         >
                           {(provided, snapshot) => (
                             <div
-                              className={`rounded-md py-12 ${
+                              className={`rounded-xl bg-gray-0 px-4 py-5 shadow-job  ${
                                 snapshot.isDragging
-                                  ? "bg-slate-300/80"
-                                  : "bg-slate-300"
+                                  ? "bg-gray-50/95"
+                                  : "bg-gray-0"
                               }`}
                               ref={provided.innerRef}
                               {...provided.draggableProps}
@@ -227,16 +309,26 @@ const TalentManagement = () => {
                                 ...provided.draggableProps.style,
                               }}
                             >
-                              {task.title}
-                              <span>
-                                {
-                                  data[
-                                    data.findIndex(
-                                      (item) => item.title === task.title,
-                                    )
-                                  ]?.tasks.length
-                                }
-                              </span>
+                              <div className="flex items-center justify-between">
+                                <div className="flex items-center gap-2">
+                                  <HeartMemoji />
+                                  <span className="SubHead1Semibold">
+                                    김잡콕
+                                  </span>
+                                  <ChevronRight />
+                                </div>
+
+                                <button>
+                                  <ArchiveTick />
+                                </button>
+                              </div>
+                              <div className="pt-4 pb-8">badge</div>
+                              <time
+                                className="Caption1Medium text-gray-300"
+                                dateTime={new Date().toLocaleDateString()}
+                              >
+                                {new Date().toLocaleDateString()}
+                              </time>
                             </div>
                           )}
                         </Draggable>

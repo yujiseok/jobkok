b49097c340087d0de4709a1a71425a1cc91426fb
eun0leee <eun0leee@gmail.com>
2023-04-04 00:57:21 +0900

    fix : 지원자 api 수정

--- src/api/applicant.ts
+++ src/api/applicant.ts
@@ -1,31 +1,11 @@
 import type { AxiosResponse } from "axios";
 import { client } from "./axios";
 
-// 지원자 등록
-// 임시작성, api 필요함
-export const applicantSubmit = async (
-  name: string,
-  phonenumber: string,
-  email: string,
-) => {
-  const { data }: AxiosResponse = await client({
-    method: "GET",
-    url: `/view/user_validation`,
-    data: {
-      applyName: name,
-      applyPhone: phonenumber,
-      applyEmail: email,
-    },
-  });
-
-  return data;
-};
-
 // 지원자 이메일 인증
 export const emailAuth = async (applyEmail: string) => {
   const { data }: AxiosResponse = await client({
     method: "GET",
-    url: `/view/user_validation`,
+    url: `/view/:recurit_id/user_validation`,
     data: {
       applyEmail: applyEmail,
     },
@@ -34,12 +14,21 @@ export const emailAuth = async (applyEmail: string) => {
   return data;
 };
 
-// 지원서 등록
-// 임시 작성
-export const submitApply = async () => {
+// 지원자 이메일 중복확인
+export const emailDuplicatecheck = async (applyEmail: string) => {
   const { data }: AxiosResponse = await client({
     method: "GET",
-    url: `/view/user_validation`,
+    url: `/view/check?email=${applyEmail}`,
+  });
+
+  return data;
+};
+
+// 지원서 등록
+export const submitApply = async () => {
+  const { data }: AxiosResponse = await client({
+    method: "POST",
+    url: `/view/submit`,
     data: {
       careerName: "회사",
       careerStart: "2023-03-26T17:07:23.668771300",

3b67868ffef8fb81c397cfae50475a7e221d23aa
yujiseok <koesijuy@gmail.com>
2023-04-05 23:11:04 +0900

    fix: `A non-serializable value was detected` 오류 해결
    - 미들웨어 등록

--- src/app/store.ts
+++ src/app/store.ts
@@ -18,6 +18,10 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);

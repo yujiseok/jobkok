64e327a2f704c6a53a581bdb2bce998e79bee5da
hyorimm <hyorimm@gmail.com>
2023-04-05 11:47:28 +0900

    refactor: Breadcrumbs 배열로 수정 (#82)

--- src/components/TalentDetail/Breadcrumbs.tsx
+++ src/components/TalentDetail/Breadcrumbs.tsx
@@ -4,15 +4,29 @@ const Breadcrumbs = () => {
   return (
     <div className="breadcrumbs absolute -top-6 right-0 flex justify-end text-sm">
       <ul>
-        <li className="SubHead2Semibold text-gray-400">
-          <Link to="/talent/management">인재 관리</Link>
-        </li>
-        <li className="SubHead2Semibold text-gray-400">
-          <Link to="/talent/status">채용 진행 현황</Link>
-        </li>
-        <li className="SubHead2Semibold">인재 상세페이지</li>
+        {breadcrumbsData.map((breadcrumb, index) => (
+          <li
+            key={index}
+            className={`SubHead2Semibold ${
+              index === breadcrumbsData.length - 1 ? "" : "text-gray-400"
+            }`}
+          >
+            {breadcrumb.link ? (
+              <Link to={breadcrumb.link}>{breadcrumb.title}</Link>
+            ) : (
+              breadcrumb.title
+            )}
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
+
 export default Breadcrumbs;
+
+const breadcrumbsData = [
+  { title: "인재 관리", link: "/talent/management" },
+  { title: "채용 진행 현황", link: "/talent/status" },
+  { title: "인재 상세페이지", link: "" },
+];

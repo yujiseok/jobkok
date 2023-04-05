5cacba7e6cd0cce6b4e6eb462d070d41eec56001
hyerani <hran9404@naver.com>
2023-04-05 21:24:09 +0900

    refactor: 선언위치 수정

--- src/app/store.ts
+++ src/app/store.ts
@@ -16,11 +16,11 @@ const reducer = combineReducers({
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-export const persistor = persistStore(store);
-
 export const store = configureStore({
   reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

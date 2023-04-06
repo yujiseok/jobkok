8e8fcc719e29d3020685a02c1c4d5b8b4ba0ab63
hyorimm <hyorimm@gmail.com>
2023-04-06 23:24:07 +0900

    feat: 검색 커스텀 훅 수정 (#81)

--- src/lib/hooks/useSearchTalent.ts
+++ src/lib/hooks/useSearchTalent.ts
@@ -1,17 +1,28 @@
-import { useRef } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { searchApplicant } from "@/api/notification";
+import useSelectForm from "./useSelectForm";
 
-const useSearchTalent = () => {
+const useSearchTalent = (recruitId: string) => {
+  const [isSearch, setIsSearch] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const searchInput = useRef<HTMLInputElement>(null);
   const applyName = searchParams.get("applyName") ?? "";
+
+  // 이름 검색으로 찾기
+  const { data: searchTalent } = useQuery({
+    queryKey: ["searchTalent", applyName, recruitId],
+    queryFn: () => searchApplicant(applyName, recruitId),
+  });
 
   const handleSearchBar = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!searchInput?.current?.value) return;
     setSearchParams({ applyName: searchInput.current.value.trim() });
+    setIsSearch(true);
   };
 
-  return { searchInput, applyName, handleSearchBar };
+  return { searchInput, applyName, handleSearchBar, searchTalent, isSearch };
 };
 export default useSearchTalent;

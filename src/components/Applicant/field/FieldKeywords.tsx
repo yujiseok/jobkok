2ffca8f2012f2d9ca54a3d17516435161b6b89db
eun0leee <eun0leee@gmail.com>
2023-04-07 00:52:39 +0900

    fix : 지원서 작성_키워드 작업중 (#31)

--- src/components/Applicant/field/FieldKeywords.tsx
+++ src/components/Applicant/field/FieldKeywords.tsx
@@ -1,16 +1,11 @@
-import { useFieldArray, useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { ReactComponent as IconSelect } from "@/assets/applicant/select.svg";
 import { KEYWORDS_CHECK } from "@/constants/applicant";
 
 const FieldKeywords = () => {
-  const { register } = useFormContext();
-  const { fields, append, remove } = useFieldArray({
-    name: "keywords",
-  });
+  const { register, watch } = useFormContext();
+  console.log(watch());
 
-  console.log(fields);
-
-  console.log(formState.isValid);
   return (
     <div className="applicant-field-box">
       <legend className="applicant-field-legend ">나의 키워드(필수)</legend>
@@ -18,7 +13,7 @@ const FieldKeywords = () => {
         해당되는 항목의 체크박스에 체크해주세요.
       </p>
       <div className="grid h-[108px] max-w-[820px] grid-cols-5 gap-2">
-        {fields.map((keyword, index) => (
+        {KEYWORDS_CHECK.map((keyword, index) => (
           <span key={index}>
             <label
               className={`SubHead2Semibold flex min-w-[137px] items-center justify-center gap-1 rounded-lg border border-gray-100 py-2.5 text-gray-500`}

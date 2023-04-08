f78cff442f84250a4764d35e7095fb2f96de6f9b
yujiseok <koesijuy@gmail.com>
2023-04-08 17:22:11 +0900

    feat: 칸반보드 적용

--- src/pages/TalentStatus/index.tsx
+++ src/pages/TalentStatus/index.tsx
@@ -1,37 +1,138 @@
+import { useEffect, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { ReactComponent as Add } from "@/assets/svg/add-icon.svg";
 import { ReactComponent as ArchiveTickBlue } from "@/assets/svg/archive-tick-blue.svg";
 import { ReactComponent as ArchiveTick } from "@/assets/svg/archive-tick.svg";
+import { ReactComponent as ArrowRight } from "@/assets/svg/arrow-right.svg";
 import { ReactComponent as Calendar } from "@/assets/svg/calendar.svg";
 import { ReactComponent as ChevronRight } from "@/assets/svg/chevron-right.svg";
 import { ReactComponent as HeartMemoji } from "@/assets/svg/heart-memoji.svg";
+import { ReactComponent as Search } from "@/assets/svg/search.svg";
 import { ReactComponent as Trash } from "@/assets/svg/trash.svg";
 import { LIMIT } from "@/constants/pagination";
+import useAllTalentQuery from "@/lib/hooks/useAllTalentQuery";
 import useDnD from "@/lib/hooks/useDnD";
+import useFormDataQuery from "@/lib/hooks/useFormDataQuery";
+import useFormList from "@/lib/hooks/useFormList";
 import usePagination from "@/lib/hooks/usePagination";
+import useTalentByProcedureQuery from "@/lib/hooks/useTalentByProcedureQuery";
+import ceilPage from "@/lib/utils/ceilPage";
 import formatDate from "@/lib/utils/formatDate";
-import makeString from "@/lib/utils/makeString";
-import talentByProcedure from "@/lib/utils/talentByProcedure";
-import type { IKanban } from "@/types/talent";
+import talentToProcedure from "@/lib/utils/talentByProcedure";
+import type { IKanbanBase, ITalent } from "@/types/talent";
 import Banner from "@components/Common/Banner";
+import ModalForLater from "@components/Common/ModalForLater";
 import InterviewBadge from "@components/Talent/InterviewBadge";
+import KanbanHeader from "@components/Talent/KanbanHeader";
 import NumberBadge from "@components/Talent/NumberBadge";
+import Pagination from "@components/Talent/Pagination";
 import PreferentialBadge from "@components/Talent/PreferentialBadge";
 import TKeywordBadge from "@components/Talent/TKeywordBadge";
 
 const TalentStatus = () => {
-  const [data, onDragEnd] = useDnD(kanbanData);
   const { page, offset, handleClick } = usePagination();
-  const totalPage = data && Math.ceil(data?.length / LIMIT);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const applyProcedure = searchParams.get("applyProcedure") ?? "전체인재";
+  const { formData, formDataError } = useFormDataQuery();
+
+  const [recruitId, handleChangeFormList] = useFormList(formData);
+
+  const [talent, setTalent] = useState<ITalent[]>([]);
+  const handleTalentChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    item: ITalent,
+  ) => {
+    if (e.target.checked && talent.length < 4) {
+      setTalent((prev) => [...prev, item]);
+    } else {
+      const newTalent = [...talent].filter(
+        (talent) => talent.applyId !== item.applyId,
+      );
+      e.target.checked = false;
+      setTalent(newTalent);
+    }
+  };
+
+  const { allTalent, allTalentRefetch } = useAllTalentQuery(recruitId);
+
+  const totalPages =
+    allTalent && allTalent.data !== null ? ceilPage(allTalent?.data.length) : 0; // 전체 인재로?
+
+  const kanbanData: IKanbanBase[] = talentToProcedure(allTalent);
+  const { onDragEnd } = useDnD(kanbanData);
+
+  const { talentByProcedure, talentByProcedureRefetch } =
+    useTalentByProcedureQuery(recruitId, applyProcedure);
+  // const { talentByProcedure: docsTalent } = useTalentByProcedureQuery(
+  //   recruitId,
+  //   "서류제출",
+  // );
+  // const { talentByProcedure: interviewTalent } = useTalentByProcedureQuery(
+  //   recruitId,
+  //   "면접",
+  // );
+  // const { talentByProcedure: finalTalent } = useTalentByProcedureQuery(
+  //   recruitId,
+  //   "최종조율",
+  // );
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSearchParams({
+      applyProcedure: e.target.value,
+    });
+  };
+
+  // console.log({ docsTalent, interviewTalent, finalTalent });
+
+  useEffect(() => {
+    if (applyProcedure === "전체인재") {
+      allTalentRefetch();
+    } else {
+      talentByProcedureRefetch({
+        queryKey: ["procedure", recruitId, applyProcedure],
+      });
+    }
+  }, [applyProcedure, recruitId]);
+
+  //  폼 없을 시
+  if (formData?.result === "FAIL") {
+    return (
+      <Banner className="h-[25rem]">
+        <div className="mx-auto flex h-full max-w-7xl flex-col items-center justify-center">
+          <h1 className="Head2Semibold">
+            안녕하세요, <span>잡콕미술학원</span>님!
+          </h1>
+          <p className="Head4/Semibold Head4Semibold mt-[6px] mb-9">
+            첫 지원서 폼 만들고 편한 채용관리하세요!
+          </p>
+          <Link
+            to="/form/new"
+            className="flex items-center gap-2 rounded-lg border border-blue-500 bg-gray-0 px-16 py-2 text-blue-500"
+          >
+            만들러 가기 <ArrowRight />
+          </Link>
+        </div>
+      </Banner>
+    );
+  }
+
+  console.log(talentByProcedure);
+
   return (
     <>
       <Banner className="h-16">
         <div className="mx-auto flex h-full max-w-7xl items-center justify-between">
-          <select className="">
-            <option>스마트스토어 상세페이지 디자이너 지원서 폼</option>
-            <option>찜된 탈락인재</option>
-            <option>영구삭제 인재</option>
+          <select
+            className="bg-transparent pr-3 outline-none"
+            onChange={handleChangeFormList}
+          >
+            {formData?.data !== null &&
+              formData?.data.map((form) => (
+                <option key={form.id} value={form.id} className="text-gray-900">
+                  {form.title}
+                </option>
+              ))}
           </select>
 
           <button className="SubHead2Semibold rounded-md bg-blue-50 py-2 px-5 text-blue-400">
@@ -39,46 +140,44 @@ const TalentStatus = () => {
           </button>
         </div>
       </Banner>
-      <section className="pt-[6.25rem]">
-        <h4 className="Head3Semibold flex items-center gap-1">
-          채용 진행 현황
-        </h4>
-        <p className="SubHead1Semibold mt-6 mb-12 text-gray-500">
-          한 눈에 칸반보드에서 인재 현황을 확인해보세요.
-        </p>
-      </section>
-      <section>
+
+      <section className="mt-20">
+        <h4 className="Head3Semibold mb-6">채용 진행 현황</h4>
+        <div className="mt-14 mb-5 flex justify-between">
+          <p className="SubHead1Semibold text-gray-400">
+            한 눈에 칸반보드에서 인재 현황을 확인해보세요. 인재카드를
+            Drag&Drop을 통해 자유롭게 이동해보세요.
+          </p>
+        </div>
+
         <DragDropContext onDragEnd={onDragEnd}>
-          {/* grid grid-cols-3 grid-rows-1 */}
           <div className="flex items-start justify-between gap-6">
-            {data.map((applicant) => (
-              <Droppable key={applicant.id} droppableId={applicant.id}>
+            {kanbanData.map((kanban) => (
+              <Droppable key={kanban.title} droppableId={kanban.title}>
                 {(provided) => (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
                     className={`flex-1 rounded-xl border border-gray-50 bg-gray-0 pl-8 pr-4 ${
-                      applicant.applicant.length ? "pb-12" : "pb-0"
+                      kanban.applicant.length ? "pb-12" : "pb-0"
                     }`}
                   >
                     <div className="flex items-center justify-between pr-4">
                       <div className="flex items-center py-5">
-                        <span className="SubHead1Semibold">
-                          {applicant.title}
-                        </span>
-                        <NumberBadge id={applicant.id}>
-                          {applicant.applicant.length}
+                        <span className="SubHead1Semibold">{kanban.title}</span>
+                        <NumberBadge procedure={kanban.title}>
+                          {kanban.applicant.length}
                         </NumberBadge>
                       </div>
-                      {applicant.id === "면접" ? (
+                      {kanban.title === "면접" ? (
                         <button>
                           <Calendar />
                         </button>
-                      ) : applicant.id === "최종조율" ? (
+                      ) : kanban.title === "최종조율" ? (
                         <label
                           htmlFor="modal"
                           className={`cursor-pointer rounded-md border bg-gray-0 px-5 py-[0.3438rem] ${
-                            applicant.applicant.length
+                            kanban.applicant.length
                               ? " border-blue-500  text-blue-500"
                               : "pointer-events-none border-gray-200 text-gray-200"
                           }`}
@@ -89,10 +188,10 @@ const TalentStatus = () => {
                     </div>
 
                     <div className="flex max-h-[54.75rem] flex-col gap-4 overflow-y-auto overflow-x-hidden py-1 pr-3">
-                      {applicant.applicant.map((item, index) => (
+                      {kanban.applicant.map((item, index) => (
                         <Draggable
-                          key={item.applyName}
-                          draggableId={item.applyName}
+                          key={item.applyId}
+                          draggableId={item.applyId?.toString() as string}
                           index={index}
                         >
                           {(provided, snapshot) => (
@@ -132,7 +231,7 @@ const TalentStatus = () => {
                                   우대사항 <span>2</span>/<span>5</span>
                                 </PreferentialBadge>
                                 <PreferentialBadge>
-                                  키워드 <span>{item.keywords.length}</span>/
+                                  키워드 <span>{item.keywordList.length}</span>/
                                   <span>5</span>
                                 </PreferentialBadge>
                               </div>
@@ -144,9 +243,9 @@ const TalentStatus = () => {
                                   {formatDate(item.createdTime)}
                                 </time>
 
-                                <InterviewBadge>
-                                  면접 D-16 20:00 예정
-                                </InterviewBadge>
+                                {/* <InterviewBadge>
+                                      면접 D-16 20:00 예정
+                                    </InterviewBadge> */}
                               </div>
                             </div>
                           )}
@@ -166,7 +265,7 @@ const TalentStatus = () => {
         <h4 className="Head3Semibold flex items-center gap-1">
           전체 인재 현황
         </h4>
-        <div className="mt-14 mb-3 flex items-center gap-4">
+        <div className="mt-14 mb-5 flex items-center gap-4">
           <h6 className="Head4Semibold">빠른 단계 추가</h6>
           <span className="SubHead1Semibold text-gray-500">
             아래에서 인재를 선택하고 빠르게 채용단계를 넘겨보세요 (한번에 최대
@@ -179,14 +278,15 @@ const TalentStatus = () => {
           py-8"
         >
           <div className="flex items-center gap-2">
-            <div className="flex items-center gap-4 rounded-lg border border-gray-50 bg-gray-0 py-[6px] px-2">
-              <HeartMemoji className="rounded-md bg-gray-50 " />
-              <p className="SubHead1Medium text-gray-900">김잡콕</p>
-            </div>
-            <div className="flex items-center gap-4 rounded-lg border border-gray-50 bg-gray-0 py-[6px] px-2">
-              <HeartMemoji className="rounded-md bg-gray-50 " />
-              <p className="SubHead1Medium text-gray-900">김잡콕</p>
-            </div>
+            {talent.map((item) => (
+              <div
+                key={item.applyId}
+                className="flex items-center gap-4 rounded-lg border border-gray-50 bg-gray-0 py-[6px] px-2"
+              >
+                <HeartMemoji className="rounded-md bg-gray-50 " />
+                <p className="SubHead1Medium text-gray-900">{item.applyName}</p>
+              </div>
+            ))}
             <p className="Head4Semibold ml-6 text-gray-600">님을</p>
           </div>
 
@@ -209,12 +309,34 @@ const TalentStatus = () => {
 
       <section className="rounded-[20px] border-[1.5px] border-gray-50 bg-gray-0 p-11">
         <div className="flex justify-between gap-4">
-          <select className="">
-            <option selected>전체 인재</option>
-            <option>찜된 탈락 인재</option>
-            <option>영구 삭제 인재</option>
+          <select
+            className="pr-2 outline-none"
+            value={applyProcedure}
+            onChange={handleChange}
+          >
+            <option value="전체인재">전체 인재</option>
+            {APPLY_PROCEDURE.map((item) => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            ))}
           </select>
-          <input type="text" placeholder="인재를 검색해보세요" className="" />
+          <form
+            // onSubmit={handleSearchBar}
+            className="SubHead1Medium mx-6 flex justify-between rounded-md bg-blue-25 text-gray-400"
+          >
+            <label htmlFor="searchBar" className="w-full py-4 px-6">
+              <input
+                id="searchBar"
+                placeholder="인재를 검색해보세요"
+                className="autofill:none h-full w-full bg-transparent focus:outline-none"
+                // ref={searchInput}
+              />
+            </label>
+            <button className="mr-6">
+              <Search />
+            </button>
+          </form>
         </div>
 
         <div className="mt-16">
@@ -232,12 +354,13 @@ const TalentStatus = () => {
               </tr>
             </thead>
             <tbody>
-              {allApplicant?.slice(offset, offset + LIMIT).map((item) => (
+              {allTalent?.data?.slice(offset, offset + LIMIT).map((item) => (
                 <tr key={item.applyId} className="border-b border-gray-50">
                   <td>
                     <input
                       type="checkbox"
-                      className="checkbox-primary checkbox"
+                      className="h-5 w-5 border-gray-400 checked:bg-blue-500"
+                      onChange={(e) => handleTalentChange(e, item)}
                     />
                   </td>
                   <th>
@@ -247,7 +370,7 @@ const TalentStatus = () => {
                   </th>
                   <td>
                     <div className="flex gap-6px">
-                      {item.keywords.map((keyword) => (
+                      {item.keywordList.map((keyword) => (
                         <TKeywordBadge key={keyword}>{keyword}</TKeywordBadge>
                       ))}
                     </div>
@@ -271,160 +394,16 @@ const TalentStatus = () => {
               ))}
             </tbody>
           </table>
-
-          <ul className="mt-12 flex justify-center gap-14">
-            <li>
-              <button
-                disabled={page === 1}
-                onClick={() => {
-                  handleClick(makeString(page - 1));
-                }}
-              >
-                Prev
-              </button>
-            </li>
-            <ul className="flex gap-8">
-              {Array(totalPage)
-                .fill(null)
-                .map((_, i) => {
-                  return (
-                    <li key={i + 1}>
-                      <button
-                        onClick={() => handleClick(makeString(i + 1))}
-                        className={`${i + 1 === page ? "text-blue-500" : ""}`}
-                      >
-                        {i + 1}
-                      </button>
-                    </li>
-                  );
-                })}
-            </ul>
-            <li>
-              <button
-                disabled={page === totalPage}
-                onClick={() => {
-                  handleClick(makeString(page + 1));
-                }}
-              >
-                Next
-              </button>
-            </li>
-          </ul>
+          <Pagination totalPages={totalPages} />
         </div>
       </section>
+      <ModalForLater id="modal" />
     </>
   );
 };
 
-const mockData = {
-  state: 200,
-  result: "success",
-  data: [
-    {
-      applyId: "1",
-      applyName: "김김김",
-      applyPhone: "010-1111-1111",
-      applyEmail: "applyTest@test.com",
-      resumeContent: "저는 홍길동 입니다 !",
-      applyPortfolio: "https://portfolio.portfolio",
-      applyProcedure: "면접진행",
-      evaluation: "나쁘지 않음",
-      pass: false,
-      createdTime: "2023-03-21T13:04:30",
-      applyDelete: false,
-      keywords: [""],
-      wish: true,
-    },
-    {
-      applyId: "2",
-      applyName: "김이박",
-      applyPhone: "010-1111-1111",
-      applyEmail: "applyTest@test.com",
-      resumeContent: "저는 길동홍 입니다 !",
-      applyPortfolio: "https://portfolio.portfolio",
-      applyProcedure: "서류제출",
-      evaluation: "나쁘지 않음",
-      pass: false,
-      createdTime: "2023-03-21T13:04:30",
-      applyDelete: false,
-      keywords: [""],
-      wish: true,
-    },
-    {
-      applyId: "3",
-      applyName: "존안",
-      applyPhone: "010-2222-2222",
-      applyEmail: "applyTest2@test.com",
-      resumeContent: "저는 홍길동 입니다 !",
-      applyPortfolio: "https://portfolio.portfolio",
-      applyProcedure: "서류제출",
-      evaluation: "별로임",
-      pass: false,
-      createdTime: "2023-03-21T13:31:30",
-      applyDelete: false,
-      keywords: [""],
-      wish: false,
-    },
-    {
-      applyId: "4",
-      applyName: "김철수",
-      applyPhone: "010-2222-2222",
-      applyEmail: "applyTest2@test.com",
-      resumeContent: "저는 홍길동 입니다 !",
-      applyPortfolio: "https://portfolio.portfolio",
-      applyProcedure: "최종조율",
-      evaluation: "별로임",
-      pass: false,
-      createdTime: "2023-03-21T13:31:30",
-      applyDelete: false,
-      keywords: [""],
-      wish: false,
-    },
-  ],
-};
-
-const kanbanData: IKanban[] = talentByProcedure(mockData);
-console.log(kanbanData);
-
-const numberArr = Array(6)
-  .fill(1)
-  .map((v, i) => i + 1);
-
-const randomNumber1 = [...numberArr].sort(() => 0.5 - Math.random());
-const randomNumber2 = [...numberArr].sort(() => 0.5 - Math.random());
 const TH_ITEMS = ["선택", "인재", "키워드", "지원일", "액션"];
 
-const allApplicant = [
-  {
-    applyId: "1",
-    applyName: "홍길동",
-    applyPhone: "010-1111-1111",
-    applyEmail: "applyTest@test.com",
-    resumeContent: "저는 홍길동 입니다 !",
-    applyPortfolio: "https://portfolio.portfolio",
-    applyProcedure: "면접",
-    evaluation: "나쁘지 않음",
-    pass: false,
-    createdTime: "2023-03-21T13:04:30",
-    applyDelete: false,
-    keywords: ["keyword1", "keyword2"],
-    wish: true,
-  },
-  {
-    applyId: "2",
-    applyName: "김민수",
-    applyPhone: "010-2222-2222",
-    applyEmail: "applyTest2@test.com",
-    resumeContent: "저는 홍길동 입니다 !",
-    applyPortfolio: "https://portfolio.portfolio",
-    applyProcedure: "서류전형",
-    evaluation: "별로임",
-    pass: false,
-    createdTime: "2023-03-21T13:31:30",
-    applyDelete: false,
-    keywords: ["keyword1", "keyword2"],
-    wish: false,
-  },
-];
+const APPLY_PROCEDURE = ["서류제출", "면접", "최종조율"];
 
 export default TalentStatus;

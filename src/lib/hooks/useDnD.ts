e1e8e28c58f041a4f7a7dfcfefdc3cdfdf8e5f4c
yujiseok <koesijuy@gmail.com>
2023-04-08 17:16:44 +0900

    refactor: useDnD 훅 수정

--- src/lib/hooks/useDnD.ts
+++ src/lib/hooks/useDnD.ts
@@ -1,39 +1,25 @@
-import { useState } from "react";
 import type { OnDragEndResponder } from "react-beautiful-dnd";
 import { editTalentByProcedure } from "@/api/talent";
-import type { IKanban } from "@/types/talent";
+import type { IKanbanBase } from "@/types/talent";
 
-// 실제 데이터로 처리 해야함
-// 현재 목데이터
-
-interface IDnDData {
-  id: string;
-  title: string;
-  tasks: {
-    id: string;
-    title: string;
-  }[];
-}
-
-type UseDnD = (
-  dndData: IKanban[],
-) => [data: IKanban[], onDragEnd: OnDragEndResponder];
+type UseDnD = (dndData: IKanbanBase[]) => { onDragEnd: OnDragEndResponder };
 
 const useDnD: UseDnD = (dndData) => {
-  const [data, setData] = useState(dndData);
-
+  // const [data, setData] = useState(dndData);
   const onDragEnd: OnDragEndResponder = async (result) => {
     if (!result.destination) return;
 
     const { source, destination } = result;
     if (source.droppableId !== destination.droppableId) {
-      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
-      const destinationColIndex = data.findIndex(
-        (e) => e.id === destination.droppableId,
+      const sourceColIndex = dndData.findIndex(
+        (e) => e.title === source.droppableId,
+      );
+      const destinationColIndex = dndData.findIndex(
+        (e) => e.title === destination.droppableId,
       );
 
-      const sourceCol = data[sourceColIndex];
-      const destinationCol = data[destinationColIndex];
+      const sourceCol = dndData[sourceColIndex];
+      const destinationCol = dndData[destinationColIndex];
 
       const sourceApplicant = [...sourceCol.applicant];
       const destinationApplicant = [...destinationCol.applicant];
@@ -41,35 +27,31 @@ const useDnD: UseDnD = (dndData) => {
       const [removed] = sourceApplicant.splice(source.index, 1);
       destinationApplicant.splice(destination.index, 0, removed);
 
-      data[sourceColIndex].applicant = sourceApplicant;
-      data[destinationColIndex].applicant = destinationApplicant;
-      // applyId
-      console.log(removed.applyId);
-
-      // procedure
-      console.log(destinationCol.title);
-      setData(data);
+      dndData[sourceColIndex].applicant = sourceApplicant;
+      dndData[destinationColIndex].applicant = destinationApplicant;
 
       const res = await editTalentByProcedure(
         removed.applyId as string,
         destinationCol.title,
       );
+    } else {
+      const sourceColIndex = dndData.findIndex(
+        (e) => e.title === source.droppableId,
+      );
 
-      console.log(res);
+      const col = dndData[sourceColIndex];
+      const copiedItems = [...col.applicant];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+
+      dndData[sourceColIndex].applicant = copiedItems;
     }
-    // else {
-    //   const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
-
-    //   const col = data[sourceColIndex];
-    //   const copiedItems = [...col.applicant];
-    //   const [removed] = copiedItems.splice(source.index, 1);
-    //   copiedItems.splice(destination.index, 0, removed);
-
-    //   data[sourceColIndex].applicant = copiedItems;
-    //   setData(data);
-    // }
   };
 
-  return [data, onDragEnd];
+  // useEffect(() => {
+  //   setData(dndData);
+  // }, [dndData]);
+
+  return { onDragEnd };
 };
 export default useDnD;

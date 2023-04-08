13a6047d73f2dac22f3f2f8e2560026462a8bcad
hyerani <hran9404@naver.com>
2023-04-08 15:37:53 +0900

    feat: 폼관리 페이지 마크업

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import type { AxiosRequestConfig } from "axios";
 import axios from "axios";
 
 const config: AxiosRequestConfig = {
-  baseURL: "https://jobkok.shop",
+  baseURL: "http://cb5d2f2b-4932-4853-a635-3028c5f2032e.mock.pstmn.io",
 };
 
 export const client = axios.create(config);

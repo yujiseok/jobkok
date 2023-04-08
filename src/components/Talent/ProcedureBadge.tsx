f2bde468450235171262beda3bff704cba9a6b63
yujiseok <koesijuy@gmail.com>
2023-04-08 17:10:56 +0900

    refactor: 배지 컴포넌트 리팩토링

--- src/components/Talent/ProcedureBadge.tsx
+++ src/components/Talent/ProcedureBadge.tsx
@@ -1,23 +1,18 @@
 import { ReactComponent as Smile } from "@/assets/svg/smile.svg";
+import { procedureColors } from "@/constants/badgeStyle";
+import type { ProcedureType } from "@/types/talent";
 
-// 분기처리 해야함
-// 서류제출 - 면접진행 - 최종조율
 const ProcedureBadge = ({
   children,
   procedure,
 }: {
   children: React.ReactNode;
-  procedure: string | null;
+  procedure: ProcedureType;
 }) => {
+  const { bgColor, textColor } = procedureColors[procedure];
   return (
     <div
-      className={`${
-        procedure === "서류전형"
-          ? "bg-badge-red text-text-on-badge-red"
-          : procedure === "면접진행"
-          ? "bg-badge-purple text-text-on-badge-purple"
-          : "bg-badge-blue text-text-on-badge-blue"
-      } Caption2Semibold rounded-lg  px-[0.375rem] py-1 `}
+      className={`${bgColor} ${textColor} Caption2Semibold rounded-lg  px-[0.375rem] py-1 `}
     >
       <div className="flex items-center gap-[0.125rem]">
         <Smile /> {children}

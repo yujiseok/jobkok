5479e603c4078b0ae8a7fbe6ec19466a7f6c1589
yujiseok <koesijuy@gmail.com>
2023-04-09 22:27:25 +0900

    refactor: 페이지네이션 분기 처리 (#89)

--- src/components/Talent/Pagination.tsx
+++ src/components/Talent/Pagination.tsx
@@ -5,44 +5,48 @@ import makeString from "@/lib/utils/makeString";
 const Pagination = ({ length }: { length: number }) => {
   const { page, handleClick } = usePagination();
 
-  const totalPages = ceilPage(length); // 전체 인재로?
+  const totalPages = length !== undefined ? ceilPage(length) : 0; // 전체 인재로?
 
   return (
     <nav className="mt-12 flex justify-center gap-14">
-      <button
-        disabled={page === 0}
-        onClick={() => {
-          handleClick(makeString(page - 1));
-        }}
-      >
-        Prev
-      </button>
+      {totalPages > 0 ? (
+        <>
+          <button
+            disabled={page === 0}
+            onClick={() => {
+              handleClick(makeString(page - 1));
+            }}
+          >
+            Prev
+          </button>
 
-      <ul className="flex gap-8">
-        {Array(totalPages)
-          .fill(null)
-          .map((_, i) => {
-            return (
-              <li key={i}>
-                <button
-                  onClick={() => handleClick(makeString(i))}
-                  className={`${i === page ? "text-blue-500" : ""}`}
-                >
-                  {i + 1}
-                </button>
-              </li>
-            );
-          })}
-      </ul>
+          <ul className="flex gap-8">
+            {Array(totalPages)
+              .fill(null)
+              .map((_, i) => {
+                return (
+                  <li key={i}>
+                    <button
+                      onClick={() => handleClick(makeString(i))}
+                      className={`${i === page ? "text-blue-500" : ""}`}
+                    >
+                      {i + 1}
+                    </button>
+                  </li>
+                );
+              })}
+          </ul>
 
-      <button
-        disabled={page === totalPages - 1}
-        onClick={() => {
-          handleClick(makeString(page + 1));
-        }}
-      >
-        Next
-      </button>
+          <button
+            disabled={page === totalPages - 1}
+            onClick={() => {
+              handleClick(makeString(page + 1));
+            }}
+          >
+            Next
+          </button>
+        </>
+      ) : null}
     </nav>
   );
 };
